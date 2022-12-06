8a70306019acddfcee5434708d41c1e6c41d0bee
handelskonig <laurenzdrago@gmail.com>
2022-12-07 00:13:31 +0100

    First frontend concept

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,31 @@
 import { Component } from '@angular/core';
+import {FormControl} from '@angular/forms';
+import {Observable} from 'rxjs';
+import {map, startWith} from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'supermarkt-finder';
+
+export class AutocompleteSimpleExample {
+  myControl = new FormControl('');
+  options: string[] = ['Schokolade', 'Bier', 'Softdrinks', 'Wasser', 'Obst', 'Gemüse', 'ToGo', 'Frühstück',
+   'Feinkost', 'Konserven', 'Gewürze', 'Fleisch', 'Milchprodukte', 'Tiefkühlscahen', 'Haushaltsgegenstaende', 'Teigwaren', 'Backsachen', 'Chips' ];
+  filteredOptions!: Observable<string[]>;
+
+  ngOnInit() {
+    this.filteredOptions = this.myControl.valueChanges.pipe(
+      startWith(''),
+      map(value => this._filter(value || '')),
+    );
+  
+  }
+
+  private _filter(value: string): string[] {
+    const filterValue = value.toLowerCase();
+
+    return this.options.filter(option => option.toLowerCase().includes(filterValue));
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,25 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { MatAutocompleteModule } from '@angular/material/autocomplete';
+import {MatInputModule} from '@angular/material/input';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 
-import { AppComponent } from './app.component';
+import { AutocompleteSimpleExample } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AutocompleteSimpleExample
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatAutocompleteModule,
+    MatInputModule,
+    ReactiveFormsModule,
+    FormsModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AutocompleteSimpleExample]
 })
 export class AppModule { }
